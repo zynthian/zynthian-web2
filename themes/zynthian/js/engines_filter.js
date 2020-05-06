af820b2d3912ea66ec6c76241554d2b2ae935d50
jofemodo <fernando@zynthian.org>
2020-05-06 20:30:36 +0200

    Implement engine category filtering.

--- themes/zynthian/js/engines_filter.js
+++ themes/zynthian/js/engines_filter.js
@@ -1,16 +1,29 @@
-function init_engines_filter(){
-	$(document).on('change', 'div#engines_filter_tag input', function () {
-		var chklist = [];
+
+function do_engines_filter() {
+		var chkcat = null;
+		$('div#engines_filter_cat input:checked').each(function() {
+			chkcat = $(this).val()
+			console.log("checked cat => " + chkcat)
+		});
+		var chktaglist = [];
 		$('div#engines_filter_tag input:checked').each(function() {
-			chklist.push($(this).val())
+			chktaglist.push($(this).val())
 		});
 		$('article.product-card').each(function() {
+			var cat = $(this).attr('data-cat')
 			var taglist = $(this).attr('data-tag').split(',')
-			let inters = taglist.filter(x => chklist.includes(x));
-			if (inters.length==chklist.length) $(this).fadeIn(400);
+			let inters = taglist.filter(x => chktaglist.includes(x));
+			if (cat==chkcat && inters.length==chktaglist.length) $(this).fadeIn(400);
 			else $(this).fadeOut(400);
 		});
-	});
 }
 
-init_engines_filter();
+$(document).on('change', 'div#engines_filter_cat input', function () {
+	do_engines_filter();
+});
+
+$(document).on('change', 'div#engines_filter_tag input', function () {
+	do_engines_filter();
+});
+
+do_engines_filter();
