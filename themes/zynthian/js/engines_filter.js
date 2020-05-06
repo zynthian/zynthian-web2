319e175b060e6eee7a8034fa6680d8170e3a13ff
jofemodo <fernando@zynthian.org>
2020-05-06 20:39:17 +0200

    Add onload filter call.

--- themes/zynthian/js/engines_filter.js
+++ themes/zynthian/js/engines_filter.js
@@ -1,21 +1,21 @@
 
 function do_engines_filter() {
-		var chkcat = null;
-		$('div#engines_filter_cat input:checked').each(function() {
-			chkcat = $(this).val()
-			console.log("checked cat => " + chkcat)
-		});
-		var chktaglist = [];
-		$('div#engines_filter_tag input:checked').each(function() {
-			chktaglist.push($(this).val())
-		});
-		$('article.product-card').each(function() {
-			var cat = $(this).attr('data-cat')
-			var taglist = $(this).attr('data-tag').split(',')
-			let inters = taglist.filter(x => chktaglist.includes(x));
-			if (cat==chkcat && inters.length==chktaglist.length) $(this).fadeIn(400);
-			else $(this).fadeOut(400);
-		});
+	var chkcat = null;
+	$('div#engines_filter_cat input:checked').each(function() {
+		chkcat = $(this).val()
+		console.log("checked cat => " + chkcat)
+	});
+	var chktaglist = [];
+	$('div#engines_filter_tag input:checked').each(function() {
+		chktaglist.push($(this).val())
+	});
+	$('article.product-card').each(function() {
+		var cat = $(this).attr('data-cat')
+		var taglist = $(this).attr('data-tag').split(',')
+		let inters = taglist.filter(x => chktaglist.includes(x));
+		if (cat==chkcat && inters.length==chktaglist.length) $(this).fadeIn(400);
+		else $(this).fadeOut(400);
+	});
 }
 
 $(document).on('change', 'div#engines_filter_cat input', function () {
@@ -26,4 +26,4 @@ $(document).on('change', 'div#engines_filter_tag input', function () {
 	do_engines_filter();
 });
 
-do_engines_filter();
+$(document).ready(do_engines_filter);
