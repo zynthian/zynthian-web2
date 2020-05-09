f19258dd970a1f0185cbd9f1aab136af78a792bd
ganeshaproduccions <info@ganeshaproduccions.com>
2020-05-09 12:46:19 +0200

    menu mobile & engines
    css para apañar la sección de engines + menú mobile.

--- themes/zynthian/js/engines_filter.js
+++ themes/zynthian/js/engines_filter.js
@@ -1,16 +1,29 @@
-function init_engines_filter(){
-	$(document).on('change', 'div#engines_filter input', function () {
-		var chklist = [];
-		$('div#engines_filter input:checked').each(function() {
-			chklist.push($(this).val())
-		});
-		$('article.product-card').each(function() {
-			var taglist = $(this).attr('data-tag').split(',')
-			let inters = taglist.filter(x => chklist.includes(x));
-			if (inters.length==chklist.length) $(this).fadeIn(400);
-			else $(this).fadeOut(400);
-		});
+
+function do_engines_filter() {
+	var chkcat = null;
+	$('div#engines_filter_cat input:checked').each(function() {
+		chkcat = $(this).val()
+		console.log("checked cat => " + chkcat)
+	});
+	var chktaglist = [];
+	$('div#engines_filter_tag input:checked').each(function() {
+		chktaglist.push($(this).val())
+	});
+	$('article.product-card').each(function() {
+		var cat = $(this).attr('data-cat')
+		var taglist = $(this).attr('data-tag').split(',')
+		let inters = taglist.filter(x => chktaglist.includes(x));
+		if (cat==chkcat && inters.length==chktaglist.length) $(this).fadeIn(400);
+		else $(this).fadeOut(400);
 	});
 }
 
-init_engines_filter();
+$(document).on('change', 'div#engines_filter_cat input', function () {
+	do_engines_filter();
+});
+
+$(document).on('change', 'div#engines_filter_tag input', function () {
+	do_engines_filter();
+});
+
+$(document).ready(do_engines_filter);
