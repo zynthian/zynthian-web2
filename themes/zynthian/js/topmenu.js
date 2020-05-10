24801e55340f65572e2afb89d069d30e6257ad4e
jofemodo <fernando@zynthian.org>
2020-05-10 02:33:35 +0200

    Fix sticky menu JS.

--- themes/zynthian/js/topmenu.js
+++ themes/zynthian/js/topmenu.js
@@ -2,25 +2,28 @@
 var last_scrolTop = 0;
 window.onscroll = function() {
 
-  if (document.body.scrollTop + document.documentElement.scrollTop > last_scrolTop) {
-    $("header.header > .portrait").slideUp();
-  } else {
-    $("header.header > .portrait").slideDown();
-  }
-  last_scrolTop = document.body.scrollTop + document.documentElement.scrollTop 
-  
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    $("header.header > .landscape").css({"background-color": "#000000"})
-    $("header.header > .portrait").css({"background-color": "#000000"})
-    $("header.header > .landscape .header__logo").css({"width": "200px"})
-    $("header.header > .landscape .header__nav .inline-list").css({"padding-top": "5px"})
-    $(".header__nav li.haschildren ul").addClass("solid_dropdown")
-  } else if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 80) {
-    $("header.header > .landscape").css({"background-color": "transparent"})
-    $("header.header > .portrait").css({"background-color": "transparent"})
-    $("header.header > .landscape .header__logo").css({"width": "240px"})
-    $("header.header > .landscape .header__nav .inline-list").css({"padding-top": "10px"})
-    $(".header__nav li.haschildren ul").removeClass("solid_dropdown")
-  }
+	if (! $("header.header > .landscape").is(':visible')){
+		if (document.body.scrollTop + document.documentElement.scrollTop > last_scrolTop) {
+		$("header.header > .portrait").slideUp();
+		} else {
+		$("header.header > .portrait").slideDown();
+		}
+		last_scrolTop = document.body.scrollTop + document.documentElement.scrollTop 
+	} else {
+		$("header.header > .portrait").hide()
+	}
+	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+		$("header.header > .landscape").css({"background-color": "#000000"})
+		$("header.header > .portrait").css({"background-color": "#000000"})
+		$("header.header > .landscape .header__logo").css({"width": "200px"})
+		$("header.header > .landscape .header__nav .inline-list").css({"padding-top": "5px"})
+		$(".header__nav li.haschildren ul").addClass("solid_dropdown")
+	} else if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 80) {
+		$("header.header > .landscape").css({"background-color": "transparent"})
+		$("header.header > .portrait").css({"background-color": "transparent"})
+		$("header.header > .landscape .header__logo").css({"width": "240px"})
+		$("header.header > .landscape .header__nav .inline-list").css({"padding-top": "10px"})
+		$(".header__nav li.haschildren ul").removeClass("solid_dropdown")
+	}
 };
 
