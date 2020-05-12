5100138f799d1168fe716146717452af65b38bee
jofemodo <fernando@zynthian.org>
2020-05-12 17:12:53 +0200

    Improve JS code and CSS.

--- themes/zynthian/js/topmenu.js
+++ themes/zynthian/js/topmenu.js
@@ -1,28 +1,29 @@
 
 var last_scrolTop = 0;
 window.onscroll = function() {
-
+	var class_trtime = 1000;
+	var fade_trtime = 400;
 	if (! $("header.header > .landscape").is(':visible')){
 
 		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-			$("header.header > .portrait").addClass("portrait_sticky", 1000)
+			$("header.header > .portrait").addClass("portrait_sticky", class_trtime)
 		} else if (document.body.scrollTop < 100 || document.documentElement.scrollTop < 100) {
-			$("header.header > .portrait").removeClass("portrait_sticky", 1000)
+			$("header.header > .portrait").removeClass("portrait_sticky", class_trtime)
 		} 
 
 		if (document.body.scrollTop + document.documentElement.scrollTop > last_scrolTop) {
-			$("header.header > .portrait").fadeOut(400);
+			$("header.header > .portrait").fadeOut(fade_trtime);
 		} else {
-			$("header.header > .portrait").fadeIn(400);
+			$("header.header > .portrait").fadeIn(fade_trtime);
 		}
 		last_scrolTop = document.body.scrollTop + document.documentElement.scrollTop
 
 	} else {
 		$("header.header > .portrait").hide()
 		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-			$("header.header > .landscape").addClass("landscape_sticky", 1000)
+			$("header.header > .landscape").addClass("landscape_sticky", class_trtime)
 		} else if (document.body.scrollTop < 100 || document.documentElement.scrollTop < 100) {
-			$("header.header > .landscape").removeClass("landscape_sticky", 1000)
+			$("header.header > .landscape").removeClass("landscape_sticky", class_trtime)
 		}
 	}
 };
