3fe71d93bbfb880d3dc9ba6265675a8c93874701
jofemodo <fernando@zynthian.org>
2020-05-12 17:05:00 +0200

    Improve sticky menu JS & CSS: add/remove class instead of animating each propierty on JS.

--- themes/zynthian/js/topmenu.js
+++ themes/zynthian/js/topmenu.js
@@ -3,27 +3,29 @@ var last_scrolTop = 0;
 window.onscroll = function() {
 
 	if (! $("header.header > .landscape").is(':visible')){
+
+		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+			$("header.header > .portrait").addClass("portrait_sticky", 1000)
+		} else if (document.body.scrollTop < 100 || document.documentElement.scrollTop < 100) {
+			$("header.header > .portrait").removeClass("portrait_sticky", 1000)
+		} 
+
 		if (document.body.scrollTop + document.documentElement.scrollTop > last_scrolTop) {
-		$("header.header > .portrait").slideUp();
+			$("header.header > .portrait").fadeOut(400);
 		} else {
-		$("header.header > .portrait").slideDown();
+			$("header.header > .portrait").fadeIn(400);
 		}
-		last_scrolTop = document.body.scrollTop + document.documentElement.scrollTop 
+		last_scrolTop = document.body.scrollTop + document.documentElement.scrollTop
+
 	} else {
 		$("header.header > .portrait").hide()
-	}
-	if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-		$("header.header > .landscape").css({"background-color": "#000000"})
-		$("header.header > .portrait").css({"background-color": "#000000"})
-		$("header.header > .landscape .header__logo").css({"width": "200px"})
-		$("header.header > .landscape .header__nav .inline-list").css({"padding-top": "5px"})
-		$(".header__nav li.haschildren ul").addClass("solid_dropdown")
-	} else if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 80) {
-		$("header.header > .landscape").css({"background-color": "transparent"})
-		$("header.header > .portrait").css({"background-color": "transparent"})
-		$("header.header > .landscape .header__logo").css({"width": "240px"})
-		$("header.header > .landscape .header__nav .inline-list").css({"padding-top": "10px"})
-		$(".header__nav li.haschildren ul").removeClass("solid_dropdown")
+		if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+			$("header.header > .landscape").addClass("landscape_sticky", 1000)
+		} else if (document.body.scrollTop < 100 || document.documentElement.scrollTop < 100) {
+			$("header.header > .landscape").removeClass("landscape_sticky", 1000)
+		}
 	}
 };
+
+window.onresize = window.onscroll;
 
